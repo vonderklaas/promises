2e0e5767af83a9cc8056bb01532820a86f879553
Nick Garbalau <garbalaunick@gmail.com>
2023-04-11 09:23:39 +0300

    Finished with AddTodoSection component & style fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,17 @@
+<script>
+	import AddTodoSection from '../components/AddTodoSection.svelte';
+</script>
+
 <div class="min-h-screen bg-olive-one p-0 selection:bg-green-two md:px-8 md:py-24">
 	<main
 		class="mx-auto min-h-screen max-w-none rounded-none bg-cream-four px-5 py-10 outline-none md:min-h-max md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-cream-four"
 	>
-		<h1 class="text-2xl font-bold text-gray-three">Dashboard</h1>
-		<div class="mt-10" />
+		<h1 class="flex items-center text-2xl font-bold text-gray-three">
+			<span>Dashboard</span>
+			<img class="w-[40px ml-3 h-[40px]" src="/favicon.png" alt="Svelte" />
+		</h1>
+		<div class="mt-10">
+			<AddTodoSection />
+		</div>
 	</main>
 </div>
