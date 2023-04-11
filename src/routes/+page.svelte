e50cf873db59295b1c09072f9c4b0b3022e5de7f
Nick Garbalau <garbalaunick@gmail.com>
2023-04-11 08:50:17 +0300

    Added and configured Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl underline text-red-500 font-bold">Hello world!</h1>
